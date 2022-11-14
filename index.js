e68ae59608f50d7b4b96bca9cbe48d08d2f7e3a0
christy313 <christycreativity@gmail.com>
2022-11-14 14:47:46 -0500

    Built models with cart, order, product and user

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hellssssso");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("db connection success"))
+  .catch((err) => console.log(err));
+
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("server is running");
+});
