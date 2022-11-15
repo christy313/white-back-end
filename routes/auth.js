e7082d8850df81561999c5396b556a93dfbb9177
christy313 <christycreativity@gmail.com>
2022-11-14 20:23:50 -0500

    Set up token and verify auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,58 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
-  // if (!username || !email || !password) res.status(500).json(error);
+  const { username, password, email } = req.body;
+  if (!username || !email || !password)
+    return res.status(400).json("please fill required field");
+
+  const hashedPassword = bcrypt.hash(password, saltRounds);
 
   const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
+    username,
+    email,
+    password: await hashedPassword,
   });
 
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
-    // console.log(savedUser);
   } catch (error) {
     res.status(500).json(error.message);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res.status(400).json("please fill required field");
+
+  try {
+    const user = await User.findOne({
+      username,
+    });
+    if (!user) return res.status(401).json("Something went wrong");
+
+    bcrypt.compare(password, user.password, (err, isSuccess) => {
+      if (err || !isSuccess)
+        return res.status(401).json("Something went wrong");
+      const { password, ...others } = user._doc;
+
+      const accessToken = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+      );
+      res.status(200).json({ ...others, accessToken });
+    });
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,25 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const saltRounds = 10;
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = bcrypt.hash(req.body.password, saltRounds);
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
 
 module.exports = router;
