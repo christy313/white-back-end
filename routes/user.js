3f890819f3c79cb6a6c982f18efae55685ebd841
christy313 <christycreativity@gmail.com>
2022-11-14 21:48:09 -0500

    Finish routes

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 const saltRounds = 10;
 
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
@@ -19,6 +23,63 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     res.status(200).json(updatedUser);
   } catch (error) {
     res.status(500).json(error.message);
+  }
+});
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("Delete user");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
